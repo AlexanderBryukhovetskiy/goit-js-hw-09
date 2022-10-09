4b43a76ea9d54d625f50013f81c818510dd6a0b4
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-10-09 23:37:31 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,21 +2,20 @@
 // Виконуй це завдання у файлах 02-timer.html і 02-timer.js. 
 //Напиши скрипт таймера, який здійснює зворотний відлік до певної дати. Такий таймер може використовуватися у блогах та інтернет-магазинах, сторінках реєстрації подій, під час технічного обслуговування тощо. Подивися демо-відео роботи таймера.
 
-// Елементи інтефрейсу
+// /+/ Елементи інтефрейсу
 // HTML містить готову розмітку таймера, поля вибору кінцевої дати і кнопку, по кліку на яку, таймер повинен запускатися. Додай мінімальне оформлення елементів інтерфейсу. 
 
-// Бібліотека flatpickr
+// /+/ Бібліотека flatpickr
 // Використовуй бібліотеку flatpickr для того, щоб дозволити користувачеві кросбраузерно вибрати кінцеву дату і час в одному елементі інтерфейсу. Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт, крім того, що описаний в документації.
 // // Описаний в документації
 // import flatpickr from "flatpickr";
 // // Додатковий імпорт стилів
 // import "flatpickr/dist/flatpickr.min.css"; 
 
-// Бібліотека очікує, що її ініціалізують на елементі input[type="text"], тому ми додали до HTML документу поле input#datetime-picker.
+// /+/ Бібліотека очікує, що її ініціалізують на елементі input[type="text"], тому ми додали до HTML документу поле input#datetime-picker.
 //<input type="text" id="datetime-picker" />
 
-// Другим аргументом функції flatpickr(selector, options) можна передати необов'язковий об'єкт параметрів. Ми підготували для тебе об'єкт, який потрібен для виконання завдання. Розберися, за що відповідає кожна властивість в документації «Options», і використовуй його у своєму коді.
-
+// /+/ Другим аргументом функції flatpickr(selector, options) можна передати необов'язковий об'єкт параметрів. Ми підготували для тебе об'єкт, який потрібен для виконання завдання. Розберися, за що відповідає кожна властивість в документації «Options», і використовуй його у своєму коді.
 // const options = {
 //     enableTime: true,
 //     time_24hr: true,
@@ -27,13 +26,13 @@
 //     },
 //   };
 
-// Вибір дати
+// /+/ Вибір дати
 // Метод onClose() з об'єкта параметрів викликається щоразу під час закриття елемента інтерфейсу, який створює flatpickr. Саме у ньому варто обробляти дату, обрану користувачем. Параметр selectedDates - це масив обраних дат, тому ми беремо перший елемент.
 
 // /+/ Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future".
 // /+/  Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
 // /+/ Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
-// Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
+// /+/ Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
 // Відлік часу
 // Натисканням на кнопку «Start» скрипт повинен обчислювати раз на секунду, скільки часу залишилось до вказаної дати, і оновлювати інтерфейс таймера, показуючи чотири цифри: дні, години, хвилини і секунди у форматі xx:xx:xx:xx.
 // Кількість днів може складатися з більше, ніж двох цифр.
@@ -41,11 +40,10 @@
 
 // Якщо таймер запущений, для того щоб вибрати нову дату і перезапустити його - необхідно перезавантажити сторінку.
 
-// Для підрахунку значень використовуй готову функцію convertMs, де ms - різниця між кінцевою і поточною датою в мілісекундах.
+//+/ Для підрахунку значень використовуй готову функцію convertMs, де ms - різниця між кінцевою і поточною датою в мілісекундах.
 
 // Функція convertMs() повертає об'єкт з розрахованим часом, що залишився до кінцевої дати. Зверни увагу, що вона не форматує результат. Тобто, якщо залишилося 4 хвилини або будь-якої іншої складової часу, то функція поверне 4, а не 04. В інтерфейсі таймера необхідно додавати 0, якщо в числі менше двох символів. Напиши функцію addLeadingZero(value), яка використовує метод padStart() і перед рендерингом інтефрейсу форматує значення.
 */
-
 
 import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
@@ -63,7 +61,7 @@ const counterSeconds = document.querySelector('[data-seconds]');
 
 let ms; // змінна для запису часу відліку у мс
 
-const timeForCounter = {};
+let timerId = null;
 
 const options = {
     enableTime: true,
@@ -95,25 +93,38 @@ const options = {
 const dataPickr = new flatpickr(selectedDates, options);
 
 function onClick () {
-    setInterval( () => {
+    
+    timerId = setInterval( () => {
 
         let currentDate = new Date();
 
         // визначення та запис у змінну часу відліку у мс
         ms = dataPickr.selectedDates[0] - currentDate;  
+       
 
             console.log('time to count, ms =', ms);
 
-        timeForCounter = convertMs(ms);
+        const timeForCounter = convertMs(ms);
+
             
-            console.log("It's timeForCounter inside setInterval :", timeForCounter);
+        console.log("It's timeForCounter inside setInterval :", timeForCounter);
     
-        counterDays.textContent = timeForCounter.days;
-        counterHours.textContent = timeForCounter.hours;
-        counterMinutes.textContent = timeForCounter.minutes;
-        counterSeconds.textContent = timeForCounter.seconds;
-    
+        counterDays.textContent = addLeadingZero(timeForCounter.days,  0);
+        counterHours.textContent = addLeadingZero(timeForCounter.hours, 0);
+        counterMinutes.textContent = addLeadingZero(timeForCounter.minutes, 0);
+        counterSeconds.textContent = addLeadingZero(timeForCounter.seconds, 0);
+
+        console.log (addLeadingZero(timeForCounter.days,  0));
+       
+        if (timeForCounter.days === 0 && timeForCounter.hours === 0 
+            && timeForCounter.minutes === 0 && timeForCounter.seconds === 0) {
+           
+           clearInterval(timerId);
+           startCounterBtn.disabled = true;
+        }
+        
     }, 1000);
+
 }
 
 // Для підрахунку значень використовуй готову функцію convertMs, де ms - різниця між кінцевою і поточною датою в мілісекундах.
@@ -134,11 +145,19 @@ function convertMs(ms) {
     // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
-    console.log("It's inside setInterval :", { days, hours, minutes, seconds } );
+   // console.log("It's inside _convertMs(ms)_ :", { days, hours, minutes, seconds } );
 
     return { days, hours, minutes, seconds };
   }
   
+function addLeadingZero(value, addingSymbols) {
+    value = value.toString().padStart(2, addingSymbols)
+}
+
+
+
+
+
 /*
   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
